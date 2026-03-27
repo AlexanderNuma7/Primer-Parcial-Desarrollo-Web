4e1f7b06e6e4c12f1c7833fabef1cdfaacba6779
AlexanderNuma7 <wilmernuma@gmail.com>
2026-03-27 17:24:27 -0500

    Confirmacion

---
+++ MotoSport/project/js/login.js
@@ -0,0 +1,24 @@
+// Credenciales predeterminadas (hardcoded)
+const CREDENCIALES = {
+    usuario: "admin",
+    contrasena: "12345"
+};
+
+const loginForm = document.getElementById("loginForm");
+const errorMsg = document.getElementById("errorMsg");
+
+loginForm.addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que el formulario se envie
+
+    const usuario = document.getElementById("username").value;
+    const contrasena = document.getElementById("password").value;
+
+    if (usuario === CREDENCIALES.usuario && contrasena === CREDENCIALES.contrasena) {
+        alert("Login exitoso");
+        errorMsg.textContent = "";
+       
+         window.location.href = "index.html";
+    } else {
+        errorMsg.textContent = "Usuario o contrasena incorrectos.";
+    }
+});
